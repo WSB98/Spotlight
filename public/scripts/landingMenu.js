f59ec560047dde4c7bc15d1f73a681d06957ac04
WSB98 <billiamsbaker@gmail.com>
2023-04-22 19:22:15 -0400

    finished potw section for desktop and mobile

--- public/scripts/landingMenu.js
+++ public/scripts/landingMenu.js
@@ -15,9 +15,9 @@ burger.addEventListener('click', async(e) => {
         navbar.style.height = '800px';
         links.style.transition = 'all ease 0.2s';
         links.innerHTML = `<ul class="linkList">
-                                <a href="#top"><li>home</li></a>
-                                <a href="#scroller"><li>about</li></a>
-                                <a><li>mission</li></a>
+                                <a href="#top" onclick="document.getElementById('burger').click()"><li>home</li></a>
+                                <a href="#scroller" onclick="document.getElementById('burger').click()"><li>about</li></a>
+                                <a href="#potwMobile" onclick="document.getElementById('burger').click()"><li>Project of the Month</li></a>
                                 <a href="login.html"><li>login</li></a>
                             </ul>`
     }
